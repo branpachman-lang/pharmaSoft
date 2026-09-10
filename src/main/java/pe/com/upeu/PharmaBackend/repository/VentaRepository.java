aba019e90a8a8e1a371b06ad847897e62ae17049
branpachman-lang <branpachman@gmail.com>
2026-09-10 08:37:15 -0500

    v1.1.1

---
+++ src/main/java/pe/com/upeu/PharmaBackend/repository/VentaRepository.java
@@ -0,0 +1,6 @@
+package pe.com.upeu.PharmaBackend.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.com.upeu.PharmaBackend.entity.Venta;
+
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
